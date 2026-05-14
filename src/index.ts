5adbf24c212ad7d5e40e4bf1a405d2d3798921de
HyperCrowd <info@hypercrowd.org>
2026-05-14 14:20:53 -0400

    minor cleanups

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
-// This is our barrel import file for you to manually curate what gets exported for module importing
-export { add } from '@/math.js'
+// This is our barrel import file for you to manually curate what gets exported
+// Cannot use path aliases here!
+export { add } from './math.js'
